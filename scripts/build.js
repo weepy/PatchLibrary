9b16ff83e5011ad5ac699e241a711a836289bf62
Jonah Fox <jonahfox@gmail.com>
2021-11-05 08:53:53 +0100

    first version        first0 first1

---
+++ scripts/build.js
@@ -0,0 +1,57 @@
+const fs = require("fs")
+const path = require("path")
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function(file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else {
+      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+    }
+  })
+
+  return arrayOfFiles
+}
+
+
+function treeList(dirPath, tree={}) {
+    const files = fs.readdirSync(dirPath)
+
+    files.forEach(file =>  {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+            tree[file] = {} 
+            treeList(dirPath + "/" + file, tree[file])
+            // getAllFiles(dirPath + "/" + file, arrayOfFiles)
+        } else {
+            tree[file] = 1 //file //path.join(__dirname, dirPath, "/", file)
+        }
+    })
+
+    return tree
+}
+
+
+
+const files = treeList(__dirname +"/../library")
+
+console.log(files)
+let version 
+try {
+    const text = fs.readFileSync(__dirname + "/../library.json").toString()
+    version = (JSON.parse(text).version||0) + 1
+}
+catch(e) {
+    version = 1
+}
+
+
+const library = {
+    version,
+    createdAt: new Date,
+    files
+}
+fs.writeFileSync(__dirname + "/../library.json", JSON.stringify(library, null, 4))
